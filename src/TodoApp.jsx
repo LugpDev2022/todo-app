f9bc99d81117a7d9e90a80e7bc3601e8ada8d47a
Lugp <lugpdev2022@gmail.com>
2022-09-21 13:06:10 -0500

    Change on title

--- src/TodoApp.jsx
+++ src/TodoApp.jsx
@@ -5,7 +5,9 @@ import { Nav } from './ui';
 const TodoApp = () => (
   <div className='container'>
     <div className='row mt-4 text-center'>
-      <h1>#todo</h1>
+      <h1>
+        <b>#todo</b>
+      </h1>
     </div>
     <Nav />
     <AppRouter />
