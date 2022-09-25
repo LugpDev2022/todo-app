27d5ea1a8f18b44618f5eba480089ab3cf41fc13
Lugp <lugpdev2022@gmail.com>
2022-09-24 20:54:26 -0500

    Add active class

--- src/ui/Nav.jsx
+++ src/ui/Nav.jsx
@@ -1,19 +1,43 @@
-import { NavLink } from 'react-router-dom';
+import { NavLink, useLocation } from 'react-router-dom';
 
-export const Nav = () => (
-  //TODO: Change Styles
-  <div className='row'>
-    <div className='nav justify-content-center mt-4 gap-3 px-0'>
-      <NavLink to='/' className='nav-link nav-item text-dark'>
-        All
-      </NavLink>
-      <NavLink to='/active' className='nav-link nav-item text-dark'>
-        Active
-      </NavLink>
+import './Nav.css';
 
-      <NavLink to='/completed' className='nav-link nav-item text-dark'>
-        Completed
-      </NavLink>
+export const Nav = () => {
+  const { pathname } = useLocation();
+
+  return (
+    <div className='row d-flex justify-content-center'>
+      <div className='nav justify-content-between mt-4 gap-3 px-0 col-12 col-md-9 col-lg-5 mx-5'>
+        <NavLink
+          to='/'
+          className={`nav-link nav-item text-dark pb-0 ${
+            pathname === '/' ? 'my-active' : ''
+          }`}
+        >
+          All
+        </NavLink>
+        <NavLink
+          to='/active'
+          className={`nav-link nav-item text-dark pb-0 ${
+            pathname === '/active' ? 'my-active' : ''
+          }`}
+        >
+          Active
+        </NavLink>
+
+        <NavLink
+          to='/completed'
+          className={`nav-link nav-item text-dark pb-0 ${
+            pathname === '/completed' ? 'my-active' : ''
+          } `}
+        >
+          Completed
+        </NavLink>
+      </div>
+
+      <div className='col-12 col-md-9 col-lg-6 px-4 mx-5'>
+        <div className='si'></div>
+      </div>
     </div>
-  </div>
-);
+  );
+};
