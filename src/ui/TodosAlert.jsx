a6ced716bf2e21c40fd9ba1f57fbce6e0f255e69
Lugp <lugpdev2022@gmail.com>
2022-09-24 19:57:49 -0500

    Change TodoAlert padding

--- src/ui/TodosAlert.jsx
+++ src/ui/TodosAlert.jsx
@@ -5,7 +5,7 @@ export const TodosAlert = () => {
 
   return (
     <div className='row mt-4 justify-content-center'>
-      <div className='col-12 col-md-9 col-lg-6'>
+      <div className='col-12 col-md-9 col-lg-6 px-4'>
         <div className='alert alert-warning'>
           You don't have {pathname.slice(1)} todos
         </div>
